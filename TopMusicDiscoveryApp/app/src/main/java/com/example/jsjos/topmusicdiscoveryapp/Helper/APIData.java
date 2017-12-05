28574fd89514186b26549a3f4a90140ee04e54c8
Josh Smith <joshner.smith@gmail.com>
2017-12-05 00:28:19 -0800

    - API Testing!

---
+++ TopMusicDiscoveryApp/app/src/main/java/com/example/jsjos/topmusicdiscoveryapp/Helper/APIData.java
@@ -0,0 +1,59 @@
+package com.example.jsjos.topmusicdiscoveryapp.Helper;
+
+import android.util.Log;
+
+import com.squareup.okhttp.Callback;
+import com.squareup.okhttp.Headers;
+import com.squareup.okhttp.MediaType;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.RequestBody;
+import com.squareup.okhttp.Response;
+
+import java.io.IOException;
+
+/**
+ * Created by jsjos on 12/4/2017.
+ */
+
+
+
+public class APIData {
+
+    private final String client_id = "66311e9bfa0648fb8d29b41ce627f985";
+    private final String secret_id = "aaf1401b0acd4af19e8a22b26683cc1b";
+    private final String grant_type = "client_credentials";
+
+    private final OkHttpClient client = new OkHttpClient();
+    private final String urlAuthorize = "https://accounts.spotify.com/api/token";
+
+    private final String DEBUGTAG = "adD";
+
+    private String ResponseString;
+
+    public String Authenticate() throws Exception {
+
+        Request request = new Request.Builder()
+                .url(urlAuthorize)
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Request request, IOException e) {
+                // Failure!
+                ResponseString = "Failure!";
+            }
+
+            @Override
+            public void onResponse(Response response) throws IOException {
+                ResponseString = response.body().string();
+            }
+        });
+        //Log.d(DEBUGTAG, responseConverted[0]);
+
+        //return responseConverted[0]; // return response
+        return ResponseString; // return response
+    }
+
+
+}
--- TopMusicDiscoveryApp/app/src/main/java/com/example/jsjos/topmusicdiscoveryapp/MainActivity.java
+++ TopMusicDiscoveryApp/app/src/main/java/com/example/jsjos/topmusicdiscoveryapp/MainActivity.java
@@ -10,6 +10,8 @@ import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
+import com.example.jsjos.topmusicdiscoveryapp.Helper.APIData;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
@@ -17,14 +19,14 @@ import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
-    public String client_id = "66311e9bfa0648fb8d29b41ce627f985"; // Your client id
-    public String client_secret = "aaf1401b0acd4af19e8a22b26683cc1b"; // Your secret
-    public String redirect_uri = "REDIRECT_URI"; // Your redirect uri
+    private final String LOGTAG = "blah";
 
     private EditText searchBox;
     public String API_URL = "https://api.spotify.com/v1/search"; // URL for API
 
     public String artistName;
+
+    APIData api;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,59 +36,23 @@ public class MainActivity extends AppCompatActivity {
         //Set API key
 
         this.searchBox = (EditText) findViewById(R.id.search_box);
+
+        // Setup API class
+        api = new APIData();
     }
 
-    public void OnClickSearch(View view) {
+    public void OnClickSearch(View view) throws Exception {
         String unformattedString = searchBox.getText().toString();
         // Need to add + in between words of the String
         unformattedString.replaceAll("\\s","+"); // Replace all whitespace with +
         // Start the search via the API
         //SearchForData.execute();
 
-
+        String test = api.Authenticate();
+        Log.e(LOGTAG, test);
 
     }
 
-    class SearchForData extends AsyncTask<Void, Void, String> {
 
-        private Exception exception;
-
-        protected void onPreExecute() {
-
-        }
-
-        protected String doInBackground(Void... urls) {
-            try { // Try the API call here
-                URL url = new URL(API_URL + "?q=" + artistName + "&type=artist");
-                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-                try {
-                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
-                    StringBuilder stringBuilder = new StringBuilder();
-                    String line;
-                    while ((line = bufferedReader.readLine()) != null) {
-                        stringBuilder.append(line).append("\n");
-                    }
-                    bufferedReader.close();
-                    // Pass the JSON that is stringBuilder.toString() into a wrapper function that will get the artist ID from it
-                }
-                finally{
-                    urlConnection.disconnect();
-                }
-            }
-            catch(Exception e) {
-                Log.e("An error has occurred", e.getMessage(), e);
-                return null;
-            }
-            return "MUST REPLACE THIS WITH THE JSON OBJECT YOU RETURN";
-        }
-
-        protected void onPostExecute(String resultResponse) {
-            if(resultResponse == null) {
-                Log.i("An error has occurred", resultResponse);
-            }
-
-            Log.i("We are done!", resultResponse);
-        }
-    }
 
 }

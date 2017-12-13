da94ff8d0d263b0befc9b9988e04c755273adfe2
Josh Smith <joshner.smith@gmail.com>
2017-12-13 12:37:52 -0800

    - Added some getter methods

--- TopMusicDiscoveryApp/app/src/main/java/com/example/jsjos/topmusicdiscoveryapp/JSONObjects/ArtistInfo.java
+++ TopMusicDiscoveryApp/app/src/main/java/com/example/jsjos/topmusicdiscoveryapp/JSONObjects/ArtistInfo.java
@@ -8,38 +8,46 @@ public class ArtistInfo {
     //@SerializedName("artists")
     public ArtistSV artists;
     public String getArtistID() {
-        return artists.items[0].id; // Return the first artist found, since we limit our search to one artist
+        return artists.getItem(0).getId(); // Return the first artist found, since we limit our search to one artist
     }
 
 
 }
 
 class ArtistSV {
-    public String href;
-    public Item[] items;
-    public int limit;
-    public String next;
-    public int offset;
-    public String previous;
-    public int total;
+    private String href;
+    private Item[] items;
+    private int limit;
+    private String next;
+    private int offset;
+    private String previous;
+    private int total;
+
+    Item getItem(int index) {
+        return this.items[index];
+    }
 
 }
 
 class Item {
-    public ExternalUrl external_urls;
-    public Follower followers;
-    public String[] genres;
-    public String href;
-    public String id;
-    public Image[] images;
-    public String name;
-    public int popularity;
-    public String type;
-    public String uri;
+    private ExternalUrl external_urls;
+    private Follower followers;
+    private String[] genres;
+    private String href;
+    private String id;
+    private Image[] images;
+    private String name;
+    private int popularity;
+    private String type;
+    private String uri;
+
+    String getId() {
+        return this.id;
+    }
 
 }
 
 class Follower {
-    public String href;
-    public int total;
+    private String href;
+    private int total;
 }
